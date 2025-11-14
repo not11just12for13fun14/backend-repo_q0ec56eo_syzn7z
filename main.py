ec4c9ccfa31630bb227fba2d35ad8035014c6976
Flames <vibe-coder@flames.com>
2025-11-14 03:55:55 +0000

    Update project prj_q0ec56eo - 2025-11-14T03:55:54.605Z

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from pydantic import BaseModel, Field
 
 app = FastAPI()
 
+# CORS: allow requests from any domain (no credentials)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
